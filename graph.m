0c7632a2924b775ad110598e8846ab0a3ebab95a
Richard Kerr <richard.m.kerr@vanderbilt.edu>
2015-04-12 14:10:22 -0500

    Training SVM classifier using matlabs built-in utility

--- graph.m
+++ graph.m
@@ -1,7 +1,11 @@
-function [ output_args ] = graph( input_args )
+function [  ] = graph( X, y )
 %GRAPH Summary of this function goes here
 %   Detailed explanation goes here
-
-
+    pos_examples = X(y==1,:);
+    neg_examples = X(y==0,:);
+    scatter(pos_examples(:,1),pos_examples(:,2));
+    hold on;
+    scatter(neg_examples(:,1),neg_examples(:,2));
+    hold off;
 end
 
