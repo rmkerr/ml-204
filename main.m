5fd15e9a51346c384ffac7b5955fe325ab852991
Richard Kerr <richard.m.kerr@vanderbilt.edu>
2015-04-15 12:26:54 -0500

    Added w vector to visualization

--- main.m
+++ main.m
@@ -4,9 +4,10 @@ load('ex6data1.mat');
 y(y==0) = -1;
 
 d = X;
+model = svmtrain2(y,X,'-t 0');
 
 figure
-
+axis equal;
 % plot training data
 hold on;
 pos = find(y==1);
@@ -19,8 +20,20 @@ hold on;
 sv = full(model.SVs);
 plot(sv(:,1),sv(:,2),'ko');
 
+%Plot w vector
+w = model.SVs' * model.sv_coef;
+b = -model.rho;
+
+if model.Label(1) == -1
+  w = -w;
+  b = -b;
+end
+
+line([0,w(1)]+1.9225,[0,w(2)]+3.4225);
+%plot(w);
+
 % now plot decision area
-[xi,yi] = meshgrid([min(X(:,1)):0.01:max(X(:,1))],[min(X(:,2)):0.01:max(X(:,2))]);
+[xi,yi] = meshgrid([-2:0.01:6],[-1:0.01:6]);
 dd = [xi(:),yi(:)];
 [predicted_label, accuracy, decision_values] = svmpredict(zeros(size(dd,1),1), dd, model);
 pos = find(predicted_label==1);
@@ -33,3 +46,5 @@ hold on;
 h2 = plot(dd(pos,1),dd(pos,2),'s','color',bluecolor,'MarkerSize',10,'MarkerEdgeColor',bluecolor,'MarkerFaceColor',bluecolor);
 uistack(h1, 'bottom');
 uistack(h2, 'bottom');
+
+
